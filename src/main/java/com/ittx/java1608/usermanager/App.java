80364b569452c13fe22ef8dff094686c2bc6eabc
ittx <2915858910@qq.com>
2017-05-23 08:51:11 +0800

    用户管理初始化

---
+++ src/main/java/com/ittx/java1608/usermanager/App.java
@@ -0,0 +1,13 @@
+package com.ittx.java1608.usermanager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
